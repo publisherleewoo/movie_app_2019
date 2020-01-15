1a0fc96654d44a19ee0a4712f29d12b57aa161c6
leewoo <dlguddn3214@naver.com>
2020-01-15 20:45:06 +0900

    Updates

---
+++ precache-manifest.7c61f31f1d77a4d1eb94c059a78f3288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ef4c8552e418fb47244b5ef10a80e9",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "8d27a14c78608c5225f8",
+    "url": "/movie_app_2019/static/css/main.ebcf8a1d.chunk.css"
+  },
+  {
+    "revision": "ed04d2658abc6d55d4df",
+    "url": "/movie_app_2019/static/js/2.c8149f3f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.c8149f3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d27a14c78608c5225f8",
+    "url": "/movie_app_2019/static/js/main.4f2ccf59.chunk.js"
+  },
+  {
+    "revision": "8aed77d5eee8dd4b260b",
+    "url": "/movie_app_2019/static/js/runtime-main.7c3f6604.js"
+  }
+]);
